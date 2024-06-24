6b96c19f5a2817c8bbda230283ca9decf09c4c8f
Jihad Chama <54805234+jihadchama@users.noreply.github.com>
2024-06-24 17:10:06 -0300

    Initial commit

---
+++ SwiftUICoordinator/SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorApp.swift
+//  SwiftUICoordinator
+//
+//  Created by Jihad Chama on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
